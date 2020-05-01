b6653bee6071895e2bb2c3d7049137a796aee705
danny-waughman <daniel.waughman@digital.hmrc.gov.uk>
2020-05-01 13:22:43 +0100

    Added template url

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,22 +3,19 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import views.html.Index
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(mcc: MessagesControllerComponents,
+                               indexView: Index) extends FrontendController(mcc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index: Action[AnyContent] = Action { implicit request =>
+    Ok(indexView())
   }
+
 }
