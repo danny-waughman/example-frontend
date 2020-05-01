e95d8e29a1655e47b4800a82f811831c5c1b20e4
danny-waughman <daniel.waughman@digital.hmrc.gov.uk>
2020-05-01 14:55:14 +0100

    Final example gov frontend

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,7 +1,9 @@
 package controllers
 
+
 import javax.inject._
-import play.api._
+import models.Payment
+import org.joda.time.LocalDate
 import play.api.mvc._
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 import views.html.Index
@@ -15,7 +17,10 @@ class HomeController @Inject()(mcc: MessagesControllerComponents,
                                indexView: Index) extends FrontendController(mcc) {
 
   def index: Action[AnyContent] = Action { implicit request =>
-    Ok(indexView())
+    Ok(indexView(List(
+      Payment(LocalDate.parse("01-01-20"), "abcd1234", 20.00),
+      Payment(LocalDate.parse("05-01-20"), "efgh5678", 50.00)
+    )))
   }
 
 }
